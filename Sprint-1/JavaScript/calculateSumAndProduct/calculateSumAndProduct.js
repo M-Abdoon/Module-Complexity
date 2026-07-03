c821214e9d7c47fe5bd12c8a94a8238da956f8b0
Mohammed Abdoon <mohammedabdoon1996@gmail.com>
2026-07-03 04:51:14 +0100

    refactored func, reduced loops

--- Sprint-1/JavaScript/calculateSumAndProduct/calculateSumAndProduct.js
+++ Sprint-1/JavaScript/calculateSumAndProduct/calculateSumAndProduct.js
@@ -18,13 +18,12 @@
  */
 export function calculateSumAndProduct(numbers) {
   let sum = 0;
-  for (const num of numbers) {
-    sum += num;
-  }
-
   let product = 1;
+
   for (const num of numbers) {
     product *= num;
+    sum += num;
+
   }
 
   return {
